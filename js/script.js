8511eebd78ad00a876cdeb4bf5996a523a264778
amadmoney <amadilyas@gmail.com>
2024-04-15 15:35:02 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-dj-paypal","displayName":"DJ PAYPAL","link":{"linkType":"LinkTypePage","href":"#!page-dj-paypal"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
